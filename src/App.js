2f8b7be4eb3cdf09a5a382a26d358079ea0e6a2e
San-Dental <foodsearcher1999@gmail.com>
2022-12-14 11:11:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+// import { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom'
+
+ 
+import whatsapp from './components/Assests/whatsapp.png'
+import './app.css'
+import Spinner from './components/pages/Spinner';
+
+ 
+const Temp = React.lazy(() => import("./components/pages/Temp"));
+const Footer = React.lazy(() => import("./components/pages/Footer"));
+
+const TopNav = React.lazy(() => import("./components/pages/TopNav"));
+
 
 function App() {
+
+  const [spinner, setSpinner] = React.useState(true);
+  React.useEffect(() => {
+   setTimeout(()=>{
+    setSpinner(false)
+   }, 2500)
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <TopNav />
+
+      {
+        spinner ?
+          (
+            <Spinner />
+          )
+          :
+          (
+
+            <div>
+              <Routes>
+       
+              
+                <Route path="/" element={<React.Suspense fallback={<Spinner />}><Temp />  </React.Suspense>} />
+                <Route path="/Spinner" element={<Spinner />} />
+              </Routes>
+
+              <Footer />
+
+
+
+              <a href='https://wa.me/918939111172?text=Hai' className='whatsapp_icon'><img src={whatsapp} /></a>
+              {/* <Help /> */}
+
+            </div>
+          )
+      }
+
     </div>
   );
 }
